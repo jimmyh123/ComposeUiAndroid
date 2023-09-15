212ed3bc59651de7bcf3b937cf6df7f279798b9b
James Henshaw <james.henshaw@freeagent.com>
2023-09-15 20:50:30 +0100

    Initial commit
    Set up single page app with topBar, BottomBar, and FAB. Add ability to show Snackbars and Toasts

---
+++ app/src/main/java/com/example/emptyactivitytest/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.emptyactivitytest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.example.emptyactivitytest.ui.MainComposable
+import com.example.emptyactivitytest.ui.theme.EmptyActivityTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+        setContent {
+            EmptyActivityTestTheme {
+                MainComposable()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EmptyActivityTestTheme {
+        Greeting("Android")
+    }
+}
