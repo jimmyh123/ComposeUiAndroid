561add8a82c84e51b9596cc50463fd6328f972be
James Henshaw <james.henshaw@freeagent.com>
2023-09-26 13:58:53 +0100

    Add other generated files

---
+++ app/src/main/java/com/example/emptyactivitytest/MyDataClasses.kt
@@ -0,0 +1,9 @@
+package com.example.emptyactivitytest
+
+import androidx.compose.ui.graphics.Color
+
+data class Android(
+    val name: String? = null,
+    val nickname: String? = null,
+    val colours: List<Color>
+)
