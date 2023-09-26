2a3a9af80cca03c961e8cdaaa4cab3bb1bc81a25
James Henshaw <james.henshaw@freeagent.com>
2023-09-26 14:00:04 +0100

    Add test tags

---
+++ app/src/main/java/com/example/emptyactivitytest/TestTags.kt
@@ -0,0 +1,11 @@
+package com.example.emptyactivitytest
+
+object TestTags {
+    const val NAVIGATION_BAR_BUTTON = "navigation_bar_button"
+    const val TOOLBAR_PLUS_SIGN = "toolbar_plus_sign"
+    const val NEW_NOTE_BUTTON = "new_note_button"
+    const val SHOW_SNACKBAR_BUTTON = "show_snackbar_button"
+    const val BOTTOM_APP_BAR = "bottom_app_bar"
+    const val LIST_OF_NOTES_TEXT = "list_of_notes_text"
+    const val LIST_OF_NOTES_GOES_HERE_TEXT = "list_of_notes_goes_here_text"
+}
