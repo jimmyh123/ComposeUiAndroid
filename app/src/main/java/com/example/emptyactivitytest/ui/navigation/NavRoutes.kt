8f7c14a2f623c95fdddb6d24ea57f95a2de4d2bc
James Henshaw <james.henshaw@freeagent.com>
2023-10-06 16:56:06 +0100

    Add NavRoutes Object and new strings

---
+++ app/src/main/java/com/example/emptyactivitytest/ui/navigation/NavRoutes.kt
@@ -0,0 +1,21 @@
+package com.example.emptyactivitytest.ui.navigation
+
+object NavRoutes {
+
+    // main app
+    const val MAIN_SCREEN = "main_screen"
+    const val SCREEN_TWO = "screen_two"
+    const val SCREEN_THREE = "screen_three"
+
+    // NavDrawer
+    const val NAVDRAWER_HOME = "navdrawer_home"
+    const val NAVDRAWER_INBOX = "navdrawer_inbox"
+    const val NAVDRAWER_FAVOURITES = "navdrawer_favourites"
+    const val NAVDRAWER_NEW_MESSAGE = "navdrawer_new_message"
+    const val NAVDRAWER_SETTINGS = "navdrawer_settings"
+    const val NAVDRAWER_TRASH = "navdrawer_trash"
+
+    // NavBar
+    const val NAVBAR_SEARCH = "navbar_search"
+    const val NAVBAR_NOTIFICATIONS = "navbar_notifications"
+}
